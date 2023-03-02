7eecee6053a4c631942d756b7e9fb61fea6ea73e
Jaden <jadenmonroe20001@outlook.com>
2023-03-02 12:19:17 -0800

    built out the rest of th basic funcitonality just needs more styling

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload } ) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite:false
+        }
+      }
       if(payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -50,6 +57,25 @@ function reducer(state, { type, payload } ) {
       }
     case ACTIONS.CLEAR:
         return {}
+    case ACTIONS.DELETE_DIGIT: 
+        if (state.overwrite) {
+          return {
+            ...state,
+            overwite: false,
+            currentOperand:null
+          }  
+        } 
+        if(state.currentOperand == null) return state
+        if(state.currentOperand.length === 1) {
+          return {... state, currentOperand: null}
+        }
+
+        return {
+          state,
+          currentOperand: state.currentOperand.slice(0, -1)
+        }
+
+
     case ACTIONS.EVALUATE: 
       if (
         state.currentOperand == null || 
@@ -60,10 +86,13 @@ function reducer(state, { type, payload } ) {
         }
         return {
           ...state,
+          overwrite: true,
           previousOperand: null,
           operation: null,
           currentOperand: evaluate(state)
         }
+
+  
   }
 }
 
@@ -97,7 +126,7 @@ function App() {
         <div className='current-operand'>{currentOperand}</div>
       </div>
       <button className='span-two'onClick={() => dispatch({type:ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({type:ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="/" dispatch={dispatch}/>
       <DigitButton digit="1" dispatch={dispatch}/>
       <DigitButton digit="2" dispatch={dispatch}/>
